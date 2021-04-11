06a1b752cca9eea37599b8007de964cb30ded4ad
Dylan Beattie <dylan@dylanbeattie.net>
2021-04-11 11:01:40 +0100

    Working gRPC demo

---
+++ GrpcDemo/GrpcGreeterClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+namespace GrpcGreeterClient {
+	class Program {
+		static async Task Main(string[] args) {
+            using var channel = GrpcChannel.ForAddress("https://workshop.ursatile.com:5001/");
+            var client =  new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "GreeterClient" };
+            var reply = await client.SayHelloAsync(request);
+            Console.WriteLine(reply.Message);
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+		}
+	}
+}
