68dc7114003fff948c5d9be7203d2db62de90362
Dylan Beattie <dylan@dylanbeattie.net>
2021-04-11 11:31:05 +0100

    gRPC greeter client in loop

--- GrpcDemo/GrpcGreeterClient/Program.cs
+++ GrpcDemo/GrpcGreeterClient/Program.cs
@@ -3,17 +3,23 @@ using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcGreeter;
-
+using System.Diagnostics;
 namespace GrpcGreeterClient {
 	class Program {
 		static async Task Main(string[] args) {
-            using var channel = GrpcChannel.ForAddress("https://workshop.ursatile.com:5001/");
-            var client =  new Greeter.GreeterClient(channel);
-            var request = new HelloRequest { Name = "GreeterClient" };
-            var reply = await client.SayHelloAsync(request);
-            Console.WriteLine(reply.Message);
-            Console.WriteLine("Press a key to exit");
-            Console.ReadKey();
+			var sw = new Stopwatch();
+			using var channel = GrpcChannel.ForAddress("https://workshop.ursatile.com:5001/");
+			var client = new Greeter.GreeterClient(channel);
+            sw.Start();
+			for (var i = 0; i < 1000; i++) {
+				var request = new HelloRequest { Name = "GreeterClient" };
+				var reply = await client.SayHelloAsync(request);
+    			Console.WriteLine(reply.Message);
+			}
+            sw.Stop();
+            Console.WriteLine($"Did 1000 messages in {sw.ElapsedMilliseconds}ms");
+			Console.WriteLine("Press a key to exit");
+			Console.ReadKey();
 		}
 	}
 }
